44e87ec491c87d5339ecfcd140b5e679b4e44ce3
hashmat00 <info@hashmat00.com>
2016-12-26 23:19:02 +0000

    added RESTFUL Routing Create, Read, SHow, Edit, Delete, Put/post  and plus added express sanitizer to remove teh scripts on description field

--- RESTFULRouting/app.js
+++ RESTFULRouting/app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose    = require("mongoose");
+var methodOverride = require("method-override");
+var expressSanitizer = require("express-sanitizer");
 
 
 mongoose.connect("mongodb://localhost/restful-app");
@@ -9,8 +11,8 @@ mongoose.connect("mongodb://localhost/restful-app");
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
-
-
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
 
 
 var BlogSchema = new mongoose.Schema({
@@ -45,6 +47,69 @@ app.get('/blogs', function(req, res){
     });
 });
 
+
+
+app.get('/blogs/new', function(req, res) {
+    res.render('new');
+});
+
+
+app.post('/blogs', function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            console.log('somethign went wrong');
+        }else{
+            res.redirect('/blogs');
+        }
+    });
+});
+
+
+app.get("/blogs/:id", function(req, res){
+   Blog.findById(req.params.id, function(err, blog){
+      if(err){
+          res.redirect("/");
+      } else {
+          res.render("show", {blog: blog});
+      }
+   });
+});
+
+
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+          res.redirect("/blogs");
+      } else {
+          res.render("edit", {blog: foundBlog});
+      } 
+    });
+    
+});
+
+app.put('/blogs/:id', function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updateBlog){
+        if(err){
+          res.redirect("/blogs");
+      } else {
+          res.redirect("/blogs/" + req.params._id);
+      } 
+  });
+});
+
+
+
+app.delete('/blogs/:id', function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err, removeBlog){
+         if(err){
+          res.redirect("/blogs");
+      } else {
+          res.redirect("/blogs/");
+      } 
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('server has started');
