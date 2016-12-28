b3af9226906d37ce4e47dead89da0669deeff64b
hashmat00 <info@hashmat00.com>
2016-12-28 19:58:59 +0000

    added Nested Comments Routes

--- Association/references.js
+++ Association/references.js
@@ -2,47 +2,30 @@ var mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost/references-app");
 
-// Post schema
-var postSchema = new mongoose.Schema({
-    title: String,
-    content: String
-});
-var Post = mongoose.model('Post',postSchema );
+var Post = require("./model/post");
+var User = require("./model/user");
 
 
-// User schema
-var userSchema = new mongoose.Schema({
-    email: String,
-    name: String,
-    posts: [
-        {
-         type: mongoose.Schema.Types.ObjectId,
-         ref: "Post"
+
+Post.create({
+    title: 'how to cook the best burger PT-4',
+    content: 'palow palow palow---------'
+}, function(err, post){
+    User.findOne({name: 'hashmat mhh'}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            foundUser.posts.push(post);
+            foundUser.save(function(err, data){
+                if(err){
+                    console.log(err);
+                }else{ 
+                    console.log(data);
+                }
+            });
         }
-        ]
+    });
 });
-var User = mongoose.model("User", userSchema);
-
-
-// Post.create({
-//     title: 'how to cook the best burger PT-3',
-//     content: 'kabob akbob kabob ---------'
-// }, function(err, post){
-//     User.findOne({name: 'hashmat mhh'}, function(err, foundUser){
-//         if(err){
-//             console.log(err);
-//         }else{
-//             foundUser.posts.push(post);
-//             foundUser.save(function(err, data){
-//                 if(err){
-//                     console.log(err);
-//                 }else{ 
-//                     console.log(data);
-//                 }
-//             });
-//         }
-//     });
-// });
 
 
 
---
+++ CampV4/seeds.js
@@ -0,0 +1,65 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+
+var Data = [
+    {
+        name: 'Post 1',
+        image: 'http://assets.barcroftmedia.com.s3-website-eu-west-1.amazonaws.com/assets/images/recent-images-11.jpg',
+        descriptions: 'this is content description'
+    },
+    {
+        name: 'Post 2',
+        image: 'http://cmc620.com/tours/wp-content/uploads/2016/07/Dubai-Photos-Images-Dubai-UAE-Pictures-800x600.jpg',
+        descriptions: 'this is content description'
+    },
+    {
+        name: 'Post 3',
+        image: 'http://assets.barcroftmedia.com.s3-website-eu-west-1.amazonaws.com/assets/images/recent-images-11.jpg',
+        descriptions: 'this is content description'
+    },
+    {
+        name: 'Post 4',
+        image: 'http://cmc620.com/tours/wp-content/uploads/2016/07/Dubai-Photos-Images-Dubai-UAE-Pictures-800x600.jpg',
+        descriptions: 'this is content description'
+    }
+    
+    ]
+
+
+function seedDb(){
+       Campground.remove({}, function(err){
+        if(err){
+            console.log(err);
+        }else{
+            console.log('campground has been removed');
+        }
+            Data.forEach(function(seed){
+                Campground.create(seed, function(err, data){
+                    if(err){
+                        console.log(err);
+                    }else{
+                        console.log('campground added');
+                            Comment.create({
+                                text: 'this is a new comment',
+                                author: 'hash'
+                            }, function(err, comment){
+                                if(err){
+                                    console.log(err);
+                                }else{
+                                   data.comments.push(comment);
+                                   data.save(); 
+                                   console.log('created new comment');
+                                }
+                             })
+                    }
+                })
+            });
+                
+        
+    });
+ 
+}
+
+
+module.exports = seedDb;
