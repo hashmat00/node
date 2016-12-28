feffe9c513d607d7c4227eddbd7a5aac38987834
hashmat00 <info@hashmat00.com>
2016-12-28 21:14:24 +0000

    added Nested Comments Routes

--- CampV4/app.js
+++ CampV4/app.js
@@ -92,7 +92,6 @@ app.post('/campgrounds', function(req, res){
 });
 
 
-// add comments
 app.get('/campgrounds/:id/comments/new', function(req, res) {
     Campground.findById(req.params.id, function(err, campground){
         if(err){
@@ -103,6 +102,56 @@ app.get('/campgrounds/:id/comments/new', function(req, res) {
     });
 });
 
+
+app.post('/campgrounds/:id/comments', function(req, res){
+    Campground.findById(req.params.id, function(err, campground) {
+        if(err){
+            console.log(err);
+            res.redirect('/campgrounds');
+        }else{
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                }else{
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect('/campgrounds/' + campground._id);
+                }
+            });
+        }
+    });
+});
+
+// // add comments
+// app.get('/campgrounds/:id/comments/new', function(req, res) {
+//     Campground.findById(req.params.id, function(err, campground){
+//         if(err){
+//             console.log(err);
+//         }else{
+//             res.render('comments/new', {campground: campground});
+//         }
+//     });
+// });
+
+
+// app.post('/campgrounds/:id/comments', function(req, res) {
+//     Campground.findById(req.params.id, function(err, campground) {
+//         if(err){
+//             console.log(err);
+//             res.redirect('/campgrounds');
+//         }else{
+//             Comment.create(req.body.comment, function(err, comment){
+//                 if(err){
+//                     console.log(err);
+//                 }else{
+//                     campground.comments.push(comment);
+//                     campground.save();
+//                     res.redirect('/campgrounds/' + campground._id );
+//                 }
+//             });
+//         }
+//     });
+// });
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server has started');
